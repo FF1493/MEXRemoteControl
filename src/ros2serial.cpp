15a787a2e8e352f3223e2cad6aaac610e16bac71
FF1493 <henrik.wittemeier@fh-bielefeld.de>
2020-12-03 10:50:56 +0100

    Added includes

--- src/ros2serial.cpp
+++ src/ros2serial.cpp
@@ -1,5 +1,14 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "Pololu.hpp"
+#include "ServoMotor.hpp"
+
+
+Pololu conn("/dev/ttyACM0",9600);
+ServoMotor Servo1(1,6000,3600,&conn);
+ServoMotor Servo2(2,6000,3600,&conn);
+ServoMotor Servo3(3,6000,3600,&conn);
+ServoMotor Servo4(4,6000,3600,&conn);
 
 
 
