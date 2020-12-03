48145e59002679c76337a4de97833ceadcf92896
FF1493 <henrik.wittemeier@fh-bielefeld.de>
2020-12-03 09:51:47 +0100

    Initial Commit

---
+++ src/ros2serial.cpp
@@ -0,0 +1,14 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include ""
+
+
+
+ros::NodeHandle nh;
+std::string com_port, baud_rate;
+uint16 servo1middle, servo1delta;
+
+int main(int argc, char **argv)
+{
+    std::cout<<"Hallo Welt"<<std::endl;
+}
